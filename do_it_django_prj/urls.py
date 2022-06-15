46bba447453a00f7a7c8e1a6d2443db28610b485
hyeon min <gusals1997@naver.com>
2022-06-15 13:38:00 +0900

    file_upload 필드만들기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -1,9 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('blog', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('', include('single_pages.urls')),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
