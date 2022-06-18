e1229d6ccd30ee4913731effc062c01bdee34e5b
hyeon min <gusals1997@naver.com>
2022-06-18 23:42:44 +0900

    카테고리

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('', views.index),
+    path('category/<str:slug>/', views.category_page),
     path('', views.PostList.as_view()),
     # path('/<int:pk>', views.single_post_page),
     path('<int:pk>/', views.PostDetail.as_view()),
