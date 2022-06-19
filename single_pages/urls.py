42bdeace027cc6ddfe76d8d38d188befb5018ddd
hyeon min <gusals1997@naver.com>
2022-06-19 22:37:06 +0900

    수정

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('about_me', views.about_me),
+    path('about_me/', views.about_me),
     path('', views.landing),
 ]
